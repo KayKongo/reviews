bbcd1552f86091ab6d76bf38cb22adad68f27c80
kaykongo <pkkongo@st.ug.edu.gh>
2025-05-04 16:46:55 +0000

    feat: first render of the reviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import reviews from "./data";
+
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+// each person object contains id, name, job, image, text
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [people, setPeople] = useState(reviews);
+
+  return (
+    <main>
+      {people.map((person) => {
+        return (
+          <div className="review">
+            <img
+              src={person.image}
+              alt={person.name}
+              className="img-container"
+            />
+            <h2 className="author">{person.name}</h2>
+            <span className="job">{person.job}</span>
+            <p className="info">{person.text}</p>
+            <div>
+              <IoIosArrowBack className="prev-btn" />
+              <IoIosArrowForward className="next-btn" />
+            </div>
+          </div>
+        );
+      })}
+    </main>
+  );
 };
 export default App;
